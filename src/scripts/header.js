9800e4dfa11c0966ac09a383dbfb68f93d3adad3
Jaiz183 <jaizepic@gmail.com>
2023-10-20 17:41:18 -0400

    Almost completed basic header.

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,3 +1,24 @@
+var i = 0;
+images = ["vac-logo.png", "sample-img.jpg"];
+
 function openNavbar() {
   alert("You've opened a navbar.");
+}
+
+function navigateRight() {
+  if (i >= images.length - 1) {
+    alert("You're all the way at the right already.");
+  } else {
+    i++;
+    document.getElementById("currentDisplay").src = `../assets/${images[i]}`;
+  }
+}
+
+function navigateLeft() {
+  if (i <= 0) {
+    alert("You're all the way at the left already.");
+  } else {
+    i--;
+    document.getElementById("currentDisplay").src = `../assets/${images[i]}`;
+  }
 }
