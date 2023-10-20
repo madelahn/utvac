01a81aa1dbdf5b608317cd38d53deac2c2f1ac6b
Jaiz183 <jaizepic@gmail.com>
2023-10-20 17:57:37 -0400

    Oops.

--- src/scripts/featured_artwork.js
+++ src/scripts/featured_artwork.js
@@ -1,5 +1,11 @@
 var i = 0;
-images = ["vac-logo.png", "sample-img.jpg"];
+images = [
+  "sample-image-1.jpg",
+  "sample-image-2.jpg",
+  "sample-image-3.jpg",
+  "sample-image-4.jpg",
+  "sample-image-5.jpg",
+];
 
 function navigateRight() {
   if (i >= images.length - 1) {
