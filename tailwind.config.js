83767b1a6a925937cfc59f40bb0858284922be40
Jaiz183 <jaizepic@gmail.com>
2023-12-30 00:07:24 -0500

    Added aliases for colours so that I can change them easier when mock-ups are complete.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/content/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark_primary: colors.slate,
+        dark_secondary: colors.white,
+        dark_tertiary: colors.blue,
+        light_primary: colors.white,
+        light_secondary: colors.slate,
+      },
+    },
   },
   plugins: [],
 };
